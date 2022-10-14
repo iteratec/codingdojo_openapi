840ef1cf233a49bfc62e6b62dba5487d865f3198
mh-it <michael.havlicek@iteratec.com>
2022-10-14 08:03:26 +0200

    database h2 init gradle setup version updated

---
+++ vet-zentral-spring/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE animal (
+                         id   INTEGER      NOT NULL AUTO_INCREMENT,
+                         name VARCHAR(128) NOT NULL,
+                         PRIMARY KEY (id)
+);
+
+INSERT INTO animal (id, name) VALUES (1, 'Unicorn');
